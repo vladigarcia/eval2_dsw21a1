ffc3ce44cb0a55f3ff1e5547e748166626d2cdc9
JairoEnoc <146741081+JairoEnoc@users.noreply.github.com>
2023-10-02 07:59:47 -0600

    cambio de nombre

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Sistema datos aval2'),
+      home: const MyHomePage(title: 'Sistema de datos aval2'),
     );
   }
 }
